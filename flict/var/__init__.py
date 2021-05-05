6e8deb27854a94e5beb1c05f86ea3c917a315178
Konrad Weihmann <kweihmann@outlook.com>
2021-05-05 23:18:54 +0200

    flict: move implementation to a subfolder
    and organize it as a structure which is capable of being packaged
    by setuptools.
    
    Further down the line this add a variable called VAR_DIR, which can be
    used by `from flict.var import VAR_DIR` to point to the installation
    path of these non-python files
    
    As the new subfolder has a name conflict with the previously available
    bash wrapper, the wrapper script gets removed - it will be generated
    by setuptools in a way more pythonic way anyway
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

---
+++ flict/var/__init__.py
@@ -0,0 +1,13 @@
+###################################################################
+#
+# SPDX-FileCopyrightText: 2021 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+###################################################################
+
+import os
+
+VAR_DIR = os.path.dirname(__file__)
+
+__all__ = []
