f16990151aeb7b625da111723a651a24f5f3c0b8
Sandklef Henrik <hesa@sandklef.com>
2021-07-27 15:27:08 +0200

    merge

--- tests/cli/license-expr.sh
+++ tests/cli/license-expr.sh
@@ -15,18 +15,20 @@ fi
 
 begin_test
 
-compare_exec     "$FLICT 'MIT'"         "MIT" 0 "MIT only"
-compare_exec     "$FLICT 'MIT and MIT'" "MIT" 0 "Simplify MIT and MIT "
-compare_exec     "$FLICT 'MIT and MIT and BSD-3-Clause'" "BSD-3-Clause AND MIT" 0 "MIT and MIT and BSD-3-Clause"
+FLICT_CMD="PYTHONPATH=. $FLICT verify -le"
+
+compare_exec     "$FLICT_CMD MIT"                            '["MIT"]' 0 "MIT only"
+compare_exec     "$FLICT_CMD 'MIT and MIT'"                  '["MIT"]' 0 "Simplify MIT and MIT "
+compare_exec     "$FLICT_CMD 'MIT and MIT and BSD-3-Clause'" '["BSD-3-Clause", "MIT"]' 0 "MIT and MIT and BSD-3-Clause"
 
 
-compare_exec     "$FLICT 'BSD'"         "BSD-3-Clause" 0 "Expand BSD to BSD-3-Clause"
-compare_exec     "$FLICT 'MIT and MIT and BSD'" "BSD-3-Clause AND MIT" 0 "MIT and .. BSD"
-compare_exec     "$FLICT 'MIT and MIT or BSD'" "BSD-3-Clause OR MIT" 0 "MIT and MIT or BSD"
+compare_exec     "$FLICT_CMD 'BSD'"                          '["BSD-3-Clause"]' 0 "Expand BSD to BSD-3-Clause"
+compare_exec     "$FLICT_CMD 'MIT and MIT and BSD'"          '["BSD-3-Clause", "MIT"]' 0 "MIT and .. BSD"
+compare_exec     "$FLICT_CMD 'MIT and MIT or BSD'"           '["BSD-3-Clause OR MIT]"' 0 "MIT and MIT or BSD"
 
-compare_exec     "$FLICT 'GPL-2.0-only'"         "GPL-2.0-only" 0 "No relicense GPL-2.0-only"
-compare_exec     "$FLICT 'GPL-2.0-only'"         "GPL-2.0-only" 0 "No relicense GPL-2.0-only"
-compare_exec     "$FLICT 'GPL-2.0-or-later'"     "GPL-2.0-only OR GPL-3.0-only" 0 "Relicense GPL-2.0-or-later"
-compare_exec     "$FLICT 'GPL-2.0-or-later and MIT'"     "MIT AND (GPL-2.0-only OR GPL-3.0-only)" 0 "Relicense GPL-2.0-or-later and MIT"
+compare_exec     "$FLICT_CMD 'GPL-2.0-only'"         "GPL-2.0-only" 0 "No relicense GPL-2.0-only"
+compare_exec     "$FLICT_CMD 'GPL-2.0-only'"         "GPL-2.0-only" 0 "No relicense GPL-2.0-only"
+compare_exec     "$FLICT_CMD 'GPL-2.0-or-later'"     "GPL-2.0-only OR GPL-3.0-only" 0 "Relicense GPL-2.0-or-later"
+compare_exec     "$FLICT_CMD 'GPL-2.0-or-later and MIT'"     "MIT AND (GPL-2.0-only OR GPL-3.0-only)" 0 "Relicense GPL-2.0-or-later and MIT"
 
 end_test
