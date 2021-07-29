042e00ed33bd5f5c411d960be263e4427e4ac763
Sandklef Henrik <hesa@sandklef.com>
2021-07-30 00:49:46 +0200

    move markdown arrows to common

---
+++ flict/flictlib/format/common.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+###################################################################
+#
+# flict - FOSS License Compatibility Tool
+#
+# SPDX-FileCopyrightText: 2021 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+###################################################################
+
+
+compat_interprets = {
+    'left': {
+        'true':       {'markdown': '--->'},
+        'false':      {'markdown': '---|'},
+        'undefined':  {'markdown': '---U'},
+        'depends':    {'markdown': '---D'},
+        'question':   {'markdown': '---Q'},
+    },
+    'right': {
+        'true':       {'markdown': '<----'},
+        'false':      {'markdown': '|--'},
+        'undefined':  {'markdown': 'U---'},
+        'depends':    {'markdown': 'D---'},
+        'question':   {'markdown': 'Q---'},
+    },
+}
