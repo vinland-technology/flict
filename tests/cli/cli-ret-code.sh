ab4d1b02c4404baa278a1684cdf0b7be1337ade8
Sandklef Henrik <hesa@sandklef.com>
2021-07-30 14:06:17 +0200

    add test script for verifying return codes

---
+++ tests/cli/cli-ret-code.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2021 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+FLICT_DIR=$(dirname ${BASH_SOURCE[0]}/)/../../
+
+FLICT="PYTHONPATH=${FLICT_DIR} ${FLICT_DIR}/flict/__main__.py "
+
+source $(dirname ${BASH_SOURCE[0]}/)/common-funs
+if [ $? -ne 0 ];
+then
+    echo "Failed reading common-funs... bailing out";
+    exit 1;
+fi
+
+begin_test
+
+#
+# tests return value alone
+# - uses jq to check JSON content
+# 
+simple_test()
+{
+    local OPTIONS="$1"
+    local EXP_RET="$2"
+    local MSG="$3"
+
+    if [ "$MSG" = "" ]
+    then
+        MSG=$OPTIONS
+    fi
+    
+    inform_n "${MSG}"
+#    echo "command: " ${FLICT} ${OPTIONS}
+    echo ${FLICT} ${OPTIONS} | bash >/dev/null  2>/dev/null 
+    RET=$?
+    compare_exit "$RET" "$EXP_RET" "Return values \"$RET\" and \"$EXP_RET\" differs" "$OPTIONS" "$MSG"
+    inform "OK"
+
+    SUCC_CNT=$(( $SUCC_CNT + 1 ))
+
+#    echo "RET:$RET"
+}    
+
+# verify
+simple_test "verify -pf ${FLICT_DIR}/example-data/europe-small.json" 0
+simple_test "verify -pf ${FLICT_DIR}/example-data" 10
+simple_test "verify -pf ${FLICT_DIR}/example-data/europe-small.json -lcc" 0 
+simple_test "verify -pf ${FLICT_DIR}/example-data/europe-small.json -lpl" 0 
+
+# simplify
+simple_test "simplify MIT" 0 
+simple_test "simplify MIT and BSD ,,," 11
+
+exit 0
+# list
+simple_test "list" 0 
+simple_test "list -g" 0
+simple_test "list -lg MIT" 0
+
+# outbound-candidate
+simple_test "outbound-candidate MIT" 0 
+simple_test "outbound-candidate MIT and GPL-2.0-only" 0 
+
+# display-compatibility
+simple_test "display-compatibility MIT" 0 
+simple_test "display-compatibility MIT BSD" 0 
+
+
+end_test
