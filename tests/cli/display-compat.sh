716f967b2ba86ea3d7e01e3e022eb79d53ed5571
Sandklef Henrik <hesa@sandklef.com>
2021-08-13 18:57:34 +0200

    remove tests with undeterministic results
    Such tests should be done from python and check the content.

--- tests/cli/display-compat.sh
+++ tests/cli/display-compat.sh
@@ -21,13 +21,3 @@ compare_exec     "$FLICT_CMD MIT"                          \
                  '{"compatibilities": [{"license": "MIT", "licenses": []}]}' \
                  0 "MIT only"
 
-compare_exec     "$FLICT_CMD GPL-2.0-only WITH Classpath-exception-2.0"                          \
-                 '{"compatibilities": [{"license": "GPL-2.0-only WITH Classpath-exception-2.0", "licenses": []}]}' \
-                 0 "GPL-2.0-only WITH Classpath-exception-2.0 only"
-
-compare_exec     "$FLICT_CMD GPL-2.0-only WITH Classpath-exception-2.0 MIT"                          \
-                 '{"compatibilities": [{"license": "MIT", "licenses": [{"license": "GPL-2.0-only WITH Classpath-exception-2.0", "compatible_right": "true", "compatible_left": "false"}]}, {"license": "GPL-2.0-only WITH Classpath-exception-2.0", "licenses": [{"license": "MIT", "compatible_right": "false", "compatible_left": "true"}]}]}' \
-                 0 "GPL-2.0-only WITH Classpath-exception-2.0 and MIT"
-
-
-
