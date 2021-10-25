227c19a886a184718aae273f5dc84a78167cd54e
Sandklef Henrik <hesa@sandklef.com>
2021-10-25 16:27:46 +0200

    typo fixed, multiple times

--- flict/flictlib/format/format.py
+++ flict/flictlib/format/format.py
@@ -14,41 +14,41 @@
 class FormatInterface:
 
     def format_support_licenses(self, supported_licenses):
-        return "default implmentation | format_support_licenses(): " + str(supported_licenses)
+        return "default implementation | format_support_licenses(): " + str(supported_licenses)
 
     def format_license_list(self, license_list):
-        return "default implmentation | format_license_list(): " + str(license_list)
+        return "default implementation | format_license_list(): " + str(license_list)
 
     def format_report(self, report):
-        return "default implmentation | format_report(): " + str(report)
+        return "default implementation | format_report(): " + str(report)
 
     def format_license_project(self, project):
-        return "default implmentation | format_license_combinations(): " + str(project)
+        return "default implementation | format_license_combinations(): " + str(project)
 
     def format_outbound_license(self, outbounds):
-        return "default implmentation | format_outbound_license(): " + str(outbounds)
+        return "default implementation | format_outbound_license(): " + str(outbounds)
 
     def format_license_combinations(self, combinations):
-        return "default implmentation | format_license_combinations(): " + str(combinations)
+        return "default implementation | format_license_combinations(): " + str(combinations)
 
     def format_compats(self, compats):
-        return "default implmentation | format_compats(): " + str(compats)
+        return "default implementation | format_compats(): " + str(compats)
 
     def format_supported_license_groups(self, license_groups):
-        return "default implmentation | format_supported_license_groups(): " + str(license_groups)
+        return "default implementation | format_supported_license_groups(): " + str(license_groups)
 
     def format_license_group(self, compatibility, license_handler, license_group, extended_licenses):
-        return "default implmentation | format_license_group(): " + str(license_group)
+        return "default implementation | format_license_group(): " + str(license_group)
 
     def format_simplified(self, license_expression, simplified):
-        return "default implmentation | format_simplified(): " + str(simplified)
+        return "default implementation | format_simplified(): " + str(simplified)
 
     def format_verified_license(self, license_expression, outbound_candidate):
-        return "default implmentation | format_verified_license(): " + str(license_expression)
+        return "default implementation | format_verified_license(): " + str(license_expression)
 
     def format_relicense_information(self, license_handler):
-        return "default implmentation | format_relicense_information(): "
+        return "default implementation | format_relicense_information(): "
 
     def format_translation_information(self, license_handler):
-        return "default implmentation | format_translation_information(): "
+        return "default implementation | format_translation_information(): "
 
