edf96f7a4f467b46ee947a6e22bb0d311bb0aac2
Sandklef Henrik <hesa@sandklef.com>
2021-11-14 16:04:36 +0100

    Add message to exception if none given
    If no error message is provided to the FlictException constructor, use
    the one specifed in ReturnCodes

--- flict/flictlib/return_codes.py
+++ flict/flictlib/return_codes.py
@@ -49,9 +49,12 @@ class ReturnCodes(Enum):
 
 class FlictException(Exception):
 
-    def __init__(self, error_code, error_message):
+    def __init__(self, error_code, error_message = None):
         self._error_code = error_code
-        self._error_message = error_message
+        if error_message is None:
+            self._error_message = self._error_code.value[1]
+        else:
+            self._error_message = error_message
 
     def error_code(self):
         return self._error_code.value[0]
