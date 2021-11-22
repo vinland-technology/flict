f5692bd7a476d04dcc119c61cc1794df3092468a
Jens Erdmann <jens.erdmann@web.de>
2021-11-22 08:58:01 +0100

    Impl: Retire FlictSetup Class
    Retire the FlictSetup class as it is no longer needed after introducing
    the FlictImpl class. Basic functionality was moved over. Singleton
    feature was removed.
    
    Signed-off-by: Jens Erdmann <jens.erdmann@web.de>

--- flict/impl.py
+++ flict/impl.py
@@ -9,7 +9,6 @@
 ###################################################################
 
 
-from flict.flictlib import logger
 from flict.flictlib.compatibility import Compatibility
 from flict.flictlib.format.factory import FormatFactory
 from flict.flictlib.license import LicenseHandler, decode_license_expression, encode_license_expression
@@ -21,48 +20,27 @@ from flict.flictlib.return_codes import FlictException, ReturnCodes
 import json
 
 
-class FlictSetup:
-
-    _instance = None
-
-    def __init__(self, license_handler, compatibility, output_format):
-        self.license_handler = license_handler
-        self.compatibility = compatibility
-        self.formatter = FormatFactory.formatter(output_format)
-
-    @staticmethod
-    def get_setup(args):
-        if FlictSetup._instance is None:
-            logger.setup(args.debug_license, args.verbose)
-
-            license_handler = LicenseHandler(args.translations_file, args.relicense_file, "")
-            compatibility = Compatibility(args.matrix_file,
-                                          args.scancode_file,
-                                          args.license_group_file,
-                                          args.extended_licenses)
-
-            FlictSetup._instance = FlictSetup(license_handler, compatibility, args.output_format)
-
-            logger.main_logger.debug(" flict_setup: " + str(FlictSetup._instance))
-        return FlictSetup._instance
-
-
 class FlictImpl:
     def __init__(self, args) -> None:
         self._args = args
-        self._setup = FlictSetup.get_setup(args)
+        self._formatter = FormatFactory.formatter(args.output_format)
+        self._license_handler = LicenseHandler(args.translations_file, args.relicense_file, "")
+        self._compatibility = Compatibility(args.matrix_file,
+                                            args.scancode_file,
+                                            args.license_group_file,
+                                            args.extended_licenses)
 
-    def _empty_project_report(self, compatibility, license_handler, licenses):
-        project = Project(None, license_handler, licenses)
-        report_object = Report(project, compatibility)
+    def _empty_project_report(self, licenses):
+        project = Project(None, self._license_handler, licenses)
+        report_object = Report(project, self._compatibility)
         return report_object.report()
 
     def simplify(self):
         lic_str = " ".join(self._args.license_expression)
 
         try:
-            license = self._setup.license_handler.license_expression_list(lic_str)
-            return self._setup.formatter.format_simplified(lic_str, license.simplified)
+            license = self._license_handler.license_expression_list(lic_str)
+            return self._formatter.format_simplified(lic_str, license.simplified)
         except:
             raise FlictException(ReturnCodes.RET_INVALID_EXPRESSSION,
                                  "Invalid expression to simplify: " + str(self._args.license_expression))
@@ -70,37 +48,35 @@ class FlictImpl:
     def list_licenses(self):
         formatted = ""
         if self._args.license_group:
-            formatted = self._setup.formatter.format_license_group(self._setup.compatibility,
-                                                                   self._setup.license_handler,
-                                                                   self._args.license_group,
-                                                                   self._args.extended_licenses)
+            formatted = self._formatter.format_license_group(self._compatibility,
+                                                             self._license_handler,
+                                                             self._args.license_group,
+                                                             self._args.extended_licenses)
         elif self._args.list_supported_license_groups:
-            supported_license_groups = self._setup.compatibility.supported_license_groups().sort()
-            formatted = self._setup.formatter.format_supported_license_groups(supported_license_groups)
+            supported_license_groups = self._compatibility.supported_license_groups().sort()
+            formatted = self._formatter.format_supported_license_groups(supported_license_groups)
         elif self._args.list_relicensing:
-            formatted = self._setup.formatter.format_relicense_information(self._setup.license_handler)
+            formatted = self._formatter.format_relicense_information(self._license_handler)
         elif self._args.list_translation:
-            formatted = self._setup.formatter.format_translation_information(self._setup.license_handler)
+            formatted = self._formatter.format_translation_information(self._license_handler)
         else:
-            formatted = self._setup.formatter.format_support_licenses(self._setup.compatibility)
+            formatted = self._formatter.format_support_licenses(self._compatibility)
         return formatted
 
     def _verify_license_expression(self):
         lic_str = " ".join(self._args.license_expression)
 
         try:
-            report = self._empty_project_report(self._setup.compatibility,
-                                                self._setup.license_handler,
-                                                lic_str)
+            report = self._empty_project_report(lic_str)
             candidates = report['compatibility_report']['compatibilities']['outbound_candidates']
-            return self._setup.formatter.format_verified_license(lic_str, candidates)
+            return self._formatter.format_verified_license(lic_str, candidates)
         except:
             raise FlictException(ReturnCodes.RET_INVALID_EXPRESSSION,
                                  f"Could not parse expression \"{str(self._args.license_expression)}\"")
 
     def _verify_project_file(self):
         try:
-            project = Project(self._args.project_file, self._setup.license_handler)
+            project = Project(self._args.project_file, self._license_handler)
         except FlictException as e:
             raise(e)
         except:
@@ -108,12 +84,12 @@ class FlictImpl:
 
         formatted = ""
         if self._args.list_project_licenses:
-            formatted = self._setup.formatter.format_license_list(list(project.license_set()))
+            formatted = self._formatter.format_license_list(list(project.license_set()))
         elif self._args.license_combination_count:
-            formatted = self._setup.formatter.format_license_combinations(project)
+            formatted = self._formatter.format_license_combinations(project)
         else:
-            report = Report(project, self._setup.compatibility)
-            formatted = self._setup.formatter.format_report(report)
+            report = Report(project, self._compatibility)
+            formatted = self._formatter.format_report(report)
 
         return formatted
 
@@ -139,7 +115,7 @@ class FlictImpl:
         _compliance_report = self._read_compliance_report(self._args.report_file)
         _policy = Policy(self._args.policy_file)
         _policy_report = _policy.report(_compliance_report)
-        return self._setup.formatter.format_policy_report(_policy_report)
+        return self._formatter.format_policy_report(_policy_report)
 
     def display_compatibility(self):
         try:
@@ -152,7 +128,7 @@ class FlictImpl:
             # build up license string from the expression string
             _licenses = []
             for lic in lic_str.split():
-                lic_list = self._setup.license_handler.translate_and_relicense(lic).replace("(", "").replace(
+                lic_list = self._license_handler.translate_and_relicense(lic).replace("(", "").replace(
                     ")", "").replace(" ", "").replace("OR", " ").replace("AND", " ").strip().split(" ")
 
                 for lic in lic_list:
@@ -161,12 +137,12 @@ class FlictImpl:
             # Diry trick to remove all duplicates
             licenses = list(set(_licenses))
 
-            compats = self._setup.compatibility.check_compatibilities(licenses, self._args.extended_licenses)
+            compats = self._compatibility.check_compatibilities(licenses, self._args.extended_licenses)
         except:
             raise FlictException(ReturnCodes.RET_INVALID_EXPRESSSION,
                                  "Could not parse license expression: " + str(self._args.license_expression))
 
-        return self._setup.formatter.format_compats(compats)
+        return self._formatter.format_compats(compats)
 
     def suggest_outbound_candidate(self):
         lic_str = " ".join(self._args.license_expression)
