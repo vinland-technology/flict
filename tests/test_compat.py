41bb5ac46e86b100be185acb4ac35f75d96fcf05
Konrad Weihmann <kweihmann@outlook.com>
2021-11-25 19:30:12 +0100

    tests: reenable disabled tests
    - use json.loads to turn lists back into comparable objects
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -32,34 +32,9 @@ class ArgsMock:
 class CompatibilityTest(unittest.TestCase):
 
     def test_compat(self):
-        # args = ArgsMock(['MIT'])
-        # ret = FlictImpl(args).display_compatibility()
-        # self.assertEqual(ret, '{"compatibilities": [{"license": "MIT", "licenses": []}]}')
-        pass
+        args = ArgsMock(['MIT'])
+        ret = FlictImpl(args).display_compatibility()
+        self.assertEqual(ret, '{"compatibilities": [{"license": "MIT", "licenses": []}]}')
 
 if __name__ == '__main__':
     unittest.main()
-
-
-# Namespace(
-#     license_group_file='/tmp/venv/lib/python3.9/site-packages/flict-0.1-py3.9.egg/flict/var/license-group.json',
-#     matrix_file='/tmp/venv/lib/python3.9/site-packages/osadl_matrix/osadl-matrix.csv',
-#     relicense_file='/tmp/venv/lib/python3.9/site-packages/flict-0.1-py3.9.egg/flict/var/relicense.json',
-#     scancode_file=None,
-#     translations_file='/tmp/venv/lib/python3.9/site-packages/flict-0.1-py3.9.egg/flict/var/translation.json',
-#     enable_scancode=False,
-#     extended_licenses=False,
-#     no_relicense=False,
-#     output=<_io.TextIOWrapper name='<stdout>' mode='w' encoding='utf-8'>,
-#     output_format='JSON',
-#     verbose=False,
-#     licenses=None,
-#     outbound_licenses=None,
-#     version=False,
-#     debug_license=False,
-#     graph=None,
-#     table=None,
-#     license_expression=['MIT'],
-#     which='display-compatibility',
-#     func=<function display_compatibility at 0x7f837ac748b0>
-# )
