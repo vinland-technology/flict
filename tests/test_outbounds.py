59b8c05fa4efd8a55821633639248c489f4c8151
Jens Erdmann <jens.erdmann@web.de>
2021-11-26 14:02:14 +0100

    tests: Convert New Tests to Pytest
    Convert newly introduced tests to pytest to avoid manually enforcing
    local imports
    
    Signed-off-by: Jens Erdmann <jens.erdmann@web.de>

--- tests/test_outbounds.py
+++ tests/test_outbounds.py
@@ -1,49 +1,40 @@
-#!/bin/python3
-
 # SPDX-FileCopyrightText: 2021 Jens Erdmann
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import json
-import unittest
+import pytest
 
 from flict.impl import FlictImpl
 from tests.args_mock import ArgsMock
 
-class OutboundTest(unittest.TestCase):
+def _test_expression(expression, result):
+    args = ArgsMock(license_expression=expression)
+    ret = FlictImpl(args).suggest_outbound_candidate()
+    assert result == json.loads(ret)
 
-    def _test_expression(self, expression, result):
-        args = ArgsMock(license_expression=expression)
-        ret = FlictImpl(args).suggest_outbound_candidate()
-        self.assertEqual(json.loads(ret), result)
+def test_outbound():
+    _test_expression(['MIT'], ['MIT'])
+    _test_expression(['MIT and MIT'], ['MIT'])
+    _test_expression(['MIT and MIT and BSD-3-Clause'], ['BSD-3-Clause', 'MIT'])
+    _test_expression(['GPL-2.0-only and (MIT or BSD-3-Clause)'], ['GPL-2.0-only'])
+    _test_expression(['GPL-2.0-only or (MIT and BSD-3-Clause)'], ['BSD-3-Clause', 'GPL-2.0-only', 'MIT'])
+    _test_expression(['GPL-2.0-only and (Apache-2.0 or MIT)'], ['GPL-2.0-only'])
+    _test_expression(['GPL-2.0-only or (Apache-2.0 and MIT)'], ['Apache-2.0', 'GPL-2.0-only', 'MIT'])
+    _test_expression(['GPL-2.0-only and Apache-2.0'], [])
+    _test_expression(['GPL-2.0-only and Apache-2.0 and MPL-2.0'], [])
+    _test_expression(['GPL-2.0-only and MPL-2.0'], ['GPL-2.0-only', 'GPL-2.0-or-later'])
+    _test_expression(['GPL-2.0-only and MPL-2.0'], ['GPL-2.0-only', 'GPL-2.0-or-later'])
+    _test_expression(['GPL-2.0-only and (MPL-2.0 or Apache-2.0) and BSD-3-Clause'],
+                    ['GPL-2.0-only', 'GPL-2.0-or-later'])
+    _test_expression(['(GPL-2.0-only or MPL-2.0) and (Apache-2.0 or BSD-3-Clause)'],
+                    ['AGPL-3.0-or-later', 'GPL-2.0-only', 'GPL-2.0-or-later', 'GPL-3.0-or-later', 'LGPL-2.1-or-later'])
 
+def test_no_relicense():
+    # args = ArgsMock(license_expression=['GPL-2.0-only and MPL-2.0'], no_relicense=True)
+    # ret = FlictImpl(args).suggest_outbound_candidate()
+    # assert ['GPL-2.0-only'] == json.loads(ret)
 
-    def test_outbound(self):
-        self._test_expression(['MIT'], ['MIT'])
-        self._test_expression(['MIT and MIT'], ['MIT'])
-        self._test_expression(['MIT and MIT and BSD-3-Clause'], ['BSD-3-Clause', 'MIT'])
-        self._test_expression(['GPL-2.0-only and (MIT or BSD-3-Clause)'], ['GPL-2.0-only'])
-        self._test_expression(['GPL-2.0-only or (MIT and BSD-3-Clause)'], ['BSD-3-Clause', 'GPL-2.0-only', 'MIT'])
-        self._test_expression(['GPL-2.0-only and (Apache-2.0 or MIT)'], ['GPL-2.0-only'])
-        self._test_expression(['GPL-2.0-only or (Apache-2.0 and MIT)'], ['Apache-2.0', 'GPL-2.0-only', 'MIT'])
-        self._test_expression(['GPL-2.0-only and Apache-2.0'], [])
-        self._test_expression(['GPL-2.0-only and Apache-2.0 and MPL-2.0'], [])
-        self._test_expression(['GPL-2.0-only and MPL-2.0'], ['GPL-2.0-only', 'GPL-2.0-or-later'])
-        self._test_expression(['GPL-2.0-only and MPL-2.0'], ['GPL-2.0-only', 'GPL-2.0-or-later'])
-        self._test_expression(['GPL-2.0-only and (MPL-2.0 or Apache-2.0) and BSD-3-Clause'],
-                              ['GPL-2.0-only', 'GPL-2.0-or-later'])
-        self._test_expression(['(GPL-2.0-only or MPL-2.0) and (Apache-2.0 or BSD-3-Clause)'],
-                              ['AGPL-3.0-or-later', 'GPL-2.0-only', 'GPL-2.0-or-later', 'GPL-3.0-or-later', 'LGPL-2.1-or-later'])
-
-    def test_no_relicense(self):
-        # args = ArgsMock(license_expression=['GPL-2.0-only and MPL-2.0'], no_relicense=True)
-        # ret = FlictImpl(args).suggest_outbound_candidate()
-        # self.assertEqual(json.loads(ret), ['GPL-2.0-only'])
-
-        args = ArgsMock(license_expression=['GPL-2.0-only and MPL-2.0'], relicense_file='')
-        ret = FlictImpl(args).suggest_outbound_candidate()
-        self.assertEqual(json.loads(ret), ['GPL-2.0-only'])
-
-
-if __name__ == '__main__':
-    unittest.main()
+    args = ArgsMock(license_expression=['GPL-2.0-only and MPL-2.0'], relicense_file='')
+    ret = FlictImpl(args).suggest_outbound_candidate()
+    assert ['GPL-2.0-only'] == json.loads(ret)
