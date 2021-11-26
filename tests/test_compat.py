e84a1b3ac3787b91955e62b9dea3fa721b5cae2d
Jens Erdmann <jens.erdmann@web.de>
2021-11-26 14:02:14 +0100

    tests: Convert New Tests to Pytest
    Convert newly introduced tests to pytest to avoid manually enforcing
    local imports
    
    Signed-off-by: Jens Erdmann <jens.erdmann@web.de>

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -1,20 +1,13 @@
-#!/bin/python3
-
 # SPDX-FileCopyrightText: 2021 Jens Erdmann
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import unittest
+import pytest
 
 from flict.impl import FlictImpl
 from tests.args_mock import ArgsMock
 
-class CompatibilityTest(unittest.TestCase):
-
-    def test_compat(self):
-        args = ArgsMock(license_expression=['MIT'])
-        ret = FlictImpl(args).display_compatibility()
-        self.assertEqual(ret, '{"compatibilities": [{"license": "MIT", "licenses": []}]}')
-
-if __name__ == '__main__':
-    unittest.main()
+def test_compat():
+    args = ArgsMock(license_expression=['MIT'])
+    ret = FlictImpl(args).display_compatibility()
+    assert '{"compatibilities": [{"license": "MIT", "licenses": []}]}' == ret
--- tests/test_simplified.py
+++ tests/test_simplified.py
@@ -1,32 +1,25 @@
-#!/bin/python3
-
 # SPDX-FileCopyrightText: 2021 Jens Erdmann
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import unittest
+import pytest
 
 from flict.impl import FlictImpl
 from tests.args_mock import ArgsMock
 
-class SimplificationTest(unittest.TestCase):
+def _test_expression(expression, result):
+    args = ArgsMock(license_expression=expression)
+    ret = FlictImpl(args).suggest_outbound_candidate()
+    assert result == ret
 
-    def _test_expression(self, expression, result):
-        args = ArgsMock(license_expression=expression)
-        ret = FlictImpl(args).suggest_outbound_candidate()
-        self.assertEqual(ret, result)
-
-    def test_simplify(self):
-        # self._test_expression(['MIT'], '{"original": "MIT", "simplified": "MIT"}')
-        # self._test_expression(['MIT and MIT'], '{"original": "MIT and MIT", "simplified": "MIT"}')
-        # self._test_expression(['MIT and MIT and BSD-3-Clause'], '{"original": "MIT and MIT and BSD-3-Clause", "simplified": "BSD-3-Clause AND MIT"}')
-        # self._test_expression(['BSD'], '{"original": "BSD", "simplified": "BSD-3-Clause"}')
-        # self._test_expression(['MIT and MIT and BSD'], '{"original": "MIT and MIT and BSD", "simplified": "BSD-3-Clause AND MIT"}')
-        # self._test_expression(['MIT and MIT or BSD'], '{"original": "MIT and MIT or BSD", "simplified": "BSD-3-Clause OR MIT"}')
-        # self._test_expression(['GPL-2.0-only'], '{"original": "GPL-2.0-only", "simplified": "GPL-2.0-only"}')
-        # self._test_expression(['GPL-2.0-or-later'], '{"original": "GPL-2.0-or-later", "simplified": "GPL-2.0-or-later"}')
-        # self._test_expression(['GPL-2.0-or-later and MIT'], '{"original": "GPL-2.0-or-later and MIT", "simplified": "GPL-2.0-or-later AND MIT"}')
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
+def test_simplify():
+    # _test_expression(['MIT'], '{"original": "MIT", "simplified": "MIT"}')
+    # _test_expression(['MIT and MIT'], '{"original": "MIT and MIT", "simplified": "MIT"}')
+    # _test_expression(['MIT and MIT and BSD-3-Clause'], '{"original": "MIT and MIT and BSD-3-Clause", "simplified": "BSD-3-Clause AND MIT"}')
+    # _test_expression(['BSD'], '{"original": "BSD", "simplified": "BSD-3-Clause"}')
+    # _test_expression(['MIT and MIT and BSD'], '{"original": "MIT and MIT and BSD", "simplified": "BSD-3-Clause AND MIT"}')
+    # _test_expression(['MIT and MIT or BSD'], '{"original": "MIT and MIT or BSD", "simplified": "BSD-3-Clause OR MIT"}')
+    # _test_expression(['GPL-2.0-only'], '{"original": "GPL-2.0-only", "simplified": "GPL-2.0-only"}')
+    # _test_expression(['GPL-2.0-or-later'], '{"original": "GPL-2.0-or-later", "simplified": "GPL-2.0-or-later"}')
+    # _test_expression(['GPL-2.0-or-later and MIT'], '{"original": "GPL-2.0-or-later and MIT", "simplified": "GPL-2.0-or-later AND MIT"}')
+    pass
