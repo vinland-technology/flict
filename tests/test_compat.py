06dcb25b8c98e5a7139c80c74fac95c044c8e776
Jens Erdmann <jens.erdmann@web.de>
2021-11-26 11:12:48 +0100

    tests: Remove unneeded imports and env settings
    Remove imports and environment settings that are not needed for the new
    tests.
    
    Signed-off-by: Jens Erdmann <jens.erdmann@web.de>

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -4,18 +4,10 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import os
-import sys
 import unittest
-
-TEST_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Add to PYTHON_PATH
-sys.path.insert(0, TEST_DIR)
 
 from flict.impl import FlictImpl
 from tests.args_mock import ArgsMock
-
 
 class CompatibilityTest(unittest.TestCase):
 
