b4eb3b09523fd0d77a12d6d543347eaab8a3e895
Jens Erdmann <jens.erdmann@web.de>
2021-11-26 20:54:12 +0100

    test_simplify: Fix test
    Fix copy past error in helper function. Enable tests to mimik tests of
    license-expr.sh.
    
    Signed-off-by: Jens Erdmann <jens.erdmann@web.de>

--- tests/test_simplified.py
+++ tests/test_simplified.py
@@ -9,17 +9,16 @@ from tests.args_mock import ArgsMock
 
 def _test_expression(expression, result):
     args = ArgsMock(license_expression=expression)
-    ret = FlictImpl(args).suggest_outbound_candidate()
+    ret = FlictImpl(args).simplify()
     assert result == ret
 
 def test_simplify():
-    # _test_expression(['MIT'], '{"original": "MIT", "simplified": "MIT"}')
-    # _test_expression(['MIT and MIT'], '{"original": "MIT and MIT", "simplified": "MIT"}')
-    # _test_expression(['MIT and MIT and BSD-3-Clause'], '{"original": "MIT and MIT and BSD-3-Clause", "simplified": "BSD-3-Clause AND MIT"}')
-    # _test_expression(['BSD'], '{"original": "BSD", "simplified": "BSD-3-Clause"}')
-    # _test_expression(['MIT and MIT and BSD'], '{"original": "MIT and MIT and BSD", "simplified": "BSD-3-Clause AND MIT"}')
-    # _test_expression(['MIT and MIT or BSD'], '{"original": "MIT and MIT or BSD", "simplified": "BSD-3-Clause OR MIT"}')
-    # _test_expression(['GPL-2.0-only'], '{"original": "GPL-2.0-only", "simplified": "GPL-2.0-only"}')
-    # _test_expression(['GPL-2.0-or-later'], '{"original": "GPL-2.0-or-later", "simplified": "GPL-2.0-or-later"}')
-    # _test_expression(['GPL-2.0-or-later and MIT'], '{"original": "GPL-2.0-or-later and MIT", "simplified": "GPL-2.0-or-later AND MIT"}')
-    pass
+    _test_expression(['MIT'], '{"original": "MIT", "simplified": "MIT"}')
+    _test_expression(['MIT and MIT'], '{"original": "MIT and MIT", "simplified": "MIT"}')
+    _test_expression(['MIT and MIT and BSD-3-Clause'], '{"original": "MIT and MIT and BSD-3-Clause", "simplified": "BSD-3-Clause AND MIT"}')
+    _test_expression(['BSD'], '{"original": "BSD", "simplified": "BSD-3-Clause"}')
+    _test_expression(['MIT and MIT and BSD'], '{"original": "MIT and MIT and BSD", "simplified": "BSD-3-Clause AND MIT"}')
+    _test_expression(['MIT and MIT or BSD'], '{"original": "MIT and MIT or BSD", "simplified": "BSD-3-Clause OR MIT"}')
+    _test_expression(['GPL-2.0-only'], '{"original": "GPL-2.0-only", "simplified": "GPL-2.0-only"}')
+    _test_expression(['GPL-2.0-or-later'], '{"original": "GPL-2.0-or-later", "simplified": "GPL-2.0-or-later"}')
+    _test_expression(['GPL-2.0-or-later and MIT'], '{"original": "GPL-2.0-or-later and MIT", "simplified": "GPL-2.0-or-later AND MIT"}')
