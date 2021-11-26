6d8967474448f52376591fed7724df46320cdec3
Jens Erdmann <jens.erdmann@web.de>
2021-11-26 10:58:20 +0100

    tests: Inroduce Mock for Argparse
    Introduce mock class for argparse. There was a mock already implemented
    in outbounds, compat and simplified tests. These are now replaced with
    the shared class.
    
    Signed-off-by: Jens Erdmann <jens.erdmann@web.de>

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -13,26 +13,14 @@ TEST_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Add to PYTHON_PATH
 sys.path.insert(0, TEST_DIR)
 
-from flict.flictlib import flict_config
 from flict.impl import FlictImpl
-
-
-class ArgsMock:
-    def __init__(self, license_expression):
-        self.output_format = 'JSON'
-        self.license_group_file = flict_config.DEFAULT_GROUP_FILE 
-        self.translations_file = flict_config.DEFAULT_TRANSLATIONS_FILE
-        self.relicense_file = flict_config.DEFAULT_RELICENSE_FILE
-        self.matrix_file = flict_config.DEFAULT_MATRIX_FILE
-        self.scancode_file = None
-        self.extended_licenses = False
-        self.license_expression = license_expression
+from tests.args_mock import ArgsMock
 
 
 class CompatibilityTest(unittest.TestCase):
 
     def test_compat(self):
-        args = ArgsMock(['MIT'])
+        args = ArgsMock(license_expression=['MIT'])
         ret = FlictImpl(args).display_compatibility()
         self.assertEqual(ret, '{"compatibilities": [{"license": "MIT", "licenses": []}]}')
 
