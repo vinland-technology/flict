06ba8c75569dd59db3d95f47939a894ff9599ad5
Jens Erdmann <jens.erdmann@web.de>
2021-12-17 23:50:34 +0100

    relicense: Minor Refactor
    Minor refactor of read_relicense_file and relicense_license by inlinging
    dictionary and list creation.
    Also replace c style string concatination with f-strings and joins.
    
    Signed-off-by: Jens Erdmann <jens.erdmann@web.de>

--- flict/flictlib/relicense.py
+++ flict/flictlib/relicense.py
@@ -17,33 +17,42 @@ from argparse import RawTextHelpFormatter
 #
 #
 #
-PROGRAM_NAME = "relicense.py"
-PROGRAM_DESCRIPTION = "Add possibe relicensing for licenses "
 PROGRAM_VERSION = "0.1"
-PROGRAM_URL = "https://github.com/vinland-technology/compliance-utils"  # TODO
-PROGRAM_COPYRIGHT = "(c) 2020 Henrik Sandklef<hesa@sandklef.com>"
-PROGRAM_LICENSE = "GPL-3.0-or-larer"
-PROGRAM_AUTHOR = "Henrik Sandklef"
-PROGRAM_SEE_ALSO = "yoga (yoda's generic aggregator)\n  yocr (yoga's compliance reporter)\n  flict (FOSS License Compatibility Tool)"
 
 DEFAULT_RELICENSE_FILE = "relicensing.json"
+
+DESCRIPTION = """
+NAME
+  relicense.py
+
+DESCRIPTION
+  Add possibe relicensing for licenses
+
+"""
+
+EPILOG = """
+AUTHOR
+  Henrik Sandklef
+
+REPORTING BUGS
+  File a ticket at https://github.com/vinland-technology/flict/issues
+
+COPYRIGHT
+  Copyright (c) 2020 Henrik Sandklef<hesa@sandklef.com>.
+  License GPL-3.0-or-larer
+
+SEE ALSO
+  yoga (yoda's generic aggregator)
+  yocr (yoga's compliance reporter)
+  flict (FOSS License Compatibility Tool)
+"""
 
 
 def parse():
 
-    description = "NAME\n  " + PROGRAM_NAME + "\n\n"
-    description = description + "DESCRIPTION\n  " + PROGRAM_DESCRIPTION + "\n\n"
-
-    epilog = ""
-    epilog = epilog + "AUTHOR\n  " + PROGRAM_AUTHOR + "\n\n"
-    epilog = epilog + "REPORTING BUGS\n  File a ticket at " + PROGRAM_URL + "\n\n"
-    epilog = epilog + "COPYRIGHT\n  Copyright " + \
-        PROGRAM_COPYRIGHT + ".\n  License " + PROGRAM_LICENSE + "\n\n"
-    epilog = epilog + "SEE ALSO\n  " + PROGRAM_SEE_ALSO + "\n\n"
-
     parser = argparse.ArgumentParser(
-        description=description,
-        epilog=epilog,
+        description=DESCRIPTION,
+        epilog=EPILOG,
         formatter_class=RawTextHelpFormatter,
     )
     parser.add_argument('-f', '--relicense-file',
@@ -69,26 +78,18 @@ def read_relicense_file(relicense_file):
         #print("item: " + str(item))
         relicense_map[item['spdx']] = item
 
-    relicense_data = {}
-    relicense_data['original'] = relicense_object
-    relicense_data['relicense_map'] = relicense_map
-
-    return relicense_data
+    return {
+        'original': relicense_object,
+        'relicense_map': relicense_map
+    }
 
 
 def relicense_license(rel_map, license_expression):
-    new_license = ""
+    new_license = []
     for spdx in license_expression.replace("(", " ( ").replace(")", " ) ").split():
         if spdx in rel_map['relicense_map']:
-            rel_license = None
-            for license in rel_map['relicense_map'][spdx]["later"]:
-                if rel_license is None:
-                    rel_license = " ( " + license
-                else:
-                    rel_license = rel_license + " OR " + license + " "
-            rel_license = rel_license + ") "
-
-            new_license = new_license + " " + rel_license
+            rel_license = ' OR '.join(rel_map['relicense_map'][spdx]["later"])
+            new_license.append(f' ( {rel_license} ) ')
         else:
-            new_license = new_license + " " + spdx
-    return new_license
+            new_license.append(spdx)
+    return ' '.join(new_license)
