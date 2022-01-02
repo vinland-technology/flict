5844254c6f70953e6d6dc5e8c81c02e8b1c69914
Jens Erdmann <jens.erdmann@web.de>
2022-01-02 23:14:06 +0100

    test_exceptions: Add Test Cases for verify
    Add test cases to ensure the verify is raising the expected exceptions.
    
    Signed-off-by: Jens Erdmann <jens.erdmann@web.de>

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2,11 +2,14 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import os
 import pytest
 
 from flict.flictlib.return_codes import FlictError, ReturnCodes
 from flict.impl import FlictImpl
 from tests.args_mock import ArgsMock
+
+TEST_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def test_exceptions():
     with pytest.raises(FlictError) as _error:
@@ -26,3 +29,24 @@ def test_exceptions():
         FlictImpl(args).display_compatibility()
 
     assert _error.value.args[0] == ReturnCodes.RET_INVALID_EXPRESSSION
+
+    with pytest.raises(FlictError) as _error:
+        prj = os.path.join(TEST_DIR, 'example-data')
+        args = ArgsMock(project_file=prj)
+        FlictImpl(args).verify()
+
+    assert _error.value.args[0] == ReturnCodes.RET_FILE_NOT_FOUND
+
+    with pytest.raises(FlictError) as _error:
+        prj = os.path.join(TEST_DIR, 'setup.py')
+        args = ArgsMock(project_file=prj)
+        FlictImpl(args).verify()
+
+    assert _error.value.args[0] == ReturnCodes.RET_INVALID_PROJECT
+
+    with pytest.raises(FlictError) as _error:
+        prj = os.path.join(TEST_DIR, 'example-data/europe-small-bad.json')
+        args = ArgsMock(project_file=prj)
+        FlictImpl(args).verify()
+
+    assert _error.value.args[0] == ReturnCodes.RET_INVALID_PROJECT
