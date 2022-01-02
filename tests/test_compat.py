c140bcb83476f363f9045e569a48bf1c97fa74db
Jens Erdmann <jens.erdmann@web.de>
2022-01-02 23:47:56 +0100

    test_compat: Move Test
    Move test to display compatibility for multiple licensens from bash to
    pytest.
    
    Signed-off-by: Jens Erdmann <jens.erdmann@web.de>

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import json
 import pytest
 
 from flict.impl import FlictImpl
@@ -11,3 +12,12 @@ def test_compat():
     args = ArgsMock(license_expression=['MIT'])
     ret = FlictImpl(args).display_compatibility()
     assert '{"compatibilities": [{"license": "MIT", "licenses": []}]}' == ret
+
+def test_complex_compat():
+    expr = ['MIT BSD GPL-2.0-only WITH Classpath-exception-2.0']    
+    args = ArgsMock(license_expression=expr)
+    ret = FlictImpl(args).display_compatibility()
+    jret = json.loads(ret)
+
+    # this can not be a direct comparism as the result is in random order as of now
+    assert len(jret['compatibilities']) == 3
