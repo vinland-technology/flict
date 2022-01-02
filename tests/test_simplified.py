638d9b54837009cc285b9b48e8df4254bf4fe0ab
Jens Erdmann <jens.erdmann@web.de>
2022-01-02 12:35:07 +0100

    cli-json Test: Move Out Tests
    Remove tests that are already covered.
    Move tests into pytests that are already handling these kind of test
    scenarios.
    
    Signed-off-by: Jens Erdmann <jens.erdmann@web.de>

--- tests/test_simplified.py
+++ tests/test_simplified.py
@@ -22,3 +22,4 @@ def test_simplify():
     _test_expression(['GPL-2.0-only'], '{"original": "GPL-2.0-only", "simplified": "GPL-2.0-only"}')
     _test_expression(['GPL-2.0-or-later'], '{"original": "GPL-2.0-or-later", "simplified": "GPL-2.0-or-later"}')
     _test_expression(['GPL-2.0-or-later and MIT'], '{"original": "GPL-2.0-or-later and MIT", "simplified": "GPL-2.0-or-later AND MIT"}')
+    _test_expression(['MIT and GPL-2.0-only WITH Classpath-exception-2.0 and MIT'], '{"original": "MIT and GPL-2.0-only WITH Classpath-exception-2.0 and MIT", "simplified": "GPL-2.0-only WITH Classpath-exception-2.0 AND MIT"}')
