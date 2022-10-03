50801e72279eccab4ebc893c4a4e13f871d8d934
Sandklef Henrik <hesa@sandklef.com>
2022-10-03 22:48:27 +0200

    Alias: Make class out of functions

--- flict/flictlib/alias.py
+++ flict/flictlib/alias.py
@@ -10,53 +10,51 @@ import re
 
 from flict.flictlib.flict_config import DEFAULT_FLICT_ALIAS_FILE
 from flict.flictlib.flict_config import VAR_DIR
+from flict.flictlib.return_codes import FlictError, ReturnCodes
 
 FLICT_ALIAS_FILE = os.path.join(VAR_DIR, DEFAULT_FLICT_ALIAS_FILE)
 
-alias_object = None
 
+class Alias:
 
-def _read_aliases(alias_file=FLICT_ALIAS_FILE):
-    global alias_object
-    with open(alias_file) as fp:
-        alias_object = json.load(fp)
+    def __init__(self, alias_file=None):
+        try:
+            with open(alias_file or FLICT_ALIAS_FILE) as fp:
+                self.alias_object = json.load(fp)
+        except:
+            raise FlictError(ReturnCodes.RET_INVALID_ALIAS_FILE,
+                             f'Could not open or parse alias file: {alias_file}')
 
+    def _replace_aliases(self, lic):
+        if not lic:
+            return None
+        for alias in self.alias_object['aliases']:
+            if lic == alias['alias']:
+                return alias['license']
+        return lic
 
-def _replace_aliases(lic):
-    if not lic:
-        return None
-    for alias in alias_object['aliases']:
-        if lic == alias['alias']:
-            return alias['license']
-    return lic
+    def replace_aliases(self, license_expr, license_file=None):
+        """Replaces license expression aliases (e.g GPLv2+) with supported
+        license identifiers (e.g. GPL-2.0-or-later)
 
+        Parameters:
+            license_expr - a list of licenses
 
-def replace_aliases(license_expr):
-    """Replaces license expression aliases (e.g GPLv2+) with supported
-    license identifiers (e.g. GPL-2.0-or-later)
+        """
+        if not license_expr:
+            return None
 
-    Parameters:
-        license_expr - a list of licenses
+        # in case we have multiple spaces separating tokens, remove them
+        license_expr = re.sub(r' +', ' ', license_expr)
 
-    """
-    if not license_expr:
-        return None
+        new_license_expr = []
+        for lic in license_expr.replace(" WITH ", "_WITH_").split():
+            new_license_expr.append(self._replace_aliases(lic.replace("_WITH_", " WITH ")))
 
-    if not alias_object:
-        _read_aliases()
+        return " ".join(new_license_expr)
 
-    # in case we have multiple spaces separating tokens, remove them
-    license_expr = re.sub(r' +', ' ', license_expr)
-
-    new_license_expr = []
-    for lic in license_expr.replace(" WITH ", "_WITH_").split():
-        new_license_expr.append(_replace_aliases(lic.replace("_WITH_", " WITH ")))
-
-    return " ".join(new_license_expr)
-
-
-def format_aliased(lic):
-    alias = replace_aliases(lic)
-    if alias != lic:
-        return lic + " (aliased to " + alias + ")"
-    return lic
+    def format_aliased(self, lic):
+        alias = self.replace_aliases(lic)
+        if alias != lic:
+            return lic + " (aliased to " + alias + ")"
+        return lic
