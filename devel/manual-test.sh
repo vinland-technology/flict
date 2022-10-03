fbfacca474d564bcbbffbc40108421f2227d6b98
Sandklef Henrik <hesa@sandklef.com>
2022-10-03 10:54:47 +0200

    Test: Fix paths

--- devel/manual-test.sh
+++ devel/manual-test.sh
@@ -37,7 +37,7 @@ flct_exec  verify -il "MIT-style" -ol GPL-3.0-only
 flct_exec  -lmf ${MERGED_MATRIX}  verify -ol "MIT-style" -il "GPL-3.0-or-later WITH GCC-exception-3.1"
 flct_exec  -of dot display-compatibility X11 BSD-3-Clause  Zlib curl > /dev/null
 flct_exec  -lmf  ${MERGED_MATRIX} -of markdown verify -f example-data/cairo-pile-flict.json 
-flct_exec  -of markdown verify -s ../license-compatibility/example-data/freetype-2.9.spdx.json -sd ../license-compatibility/example-data/ 
+flct_exec  -of markdown verify -s ./example-data/freetype-2.9.spdx.json -sd .example-data/ 
 flct_exec  simplify MIT AND X11 AND MIT AND MIT
 
 PYTHONPATH=.: ./flict/__main__.py -lmf  ${MERGED_MATRIX}  -of  dot display-compatibility  X11 BSD-3-Clause "GPL-3.0-or-later WITH GCC-exception-3.1" Zlib curl > /dev/null
