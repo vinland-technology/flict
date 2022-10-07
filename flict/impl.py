a720f1e15880ed81ba37ef308dd719549dd959fd
Sandklef Henrik <hesa@sandklef.com>
2022-10-07 17:15:29 +0200

    Create arbiter in __init__

--- flict/impl.py
+++ flict/impl.py
@@ -23,28 +23,25 @@ class FlictImpl:
 
     def __init__(self, args) -> None:
         self._args = args
+        self.arbiter = self._get_arbiter()
         self._formatter = FormatterFactory.formatter(args.output_format)
 
     def merge_license_db(self):
-        arbiter = self._get_arbiter()
-        return arbiter.extend_license_db(self._args.license_file)
+        return self.arbiter.extend_license_db(self._args.license_file)
 
     def display_compatibility(self):
-        arbiter = self._get_arbiter()
-        inter_compats = arbiter.check_compatibilities(self._args.license_expression)
+        inter_compats = self.arbiter.check_compatibilities(self._args.license_expression)
         return self._formatter.format_compats(inter_compats)
 
     def simplify(self):
-        arbiter = self._get_arbiter()
-        return arbiter.simplify_license(" ".join(self._args.license_expression))
+        return self.arbiter.simplify_license(" ".join(self._args.license_expression))
 
     def suggest_outbound_candidate(self):
-        arbiter = self._get_arbiter()
-        licenses = arbiter.licenses(" ".join(self._args.license_expression))
+        licenses = self.arbiter.licenses(" ".join(self._args.license_expression))
         outbounds = []
         try:
             for outbound in licenses:
-                compats = arbiter.inbounds_outbound_check(outbound, self._args.license_expression)
+                compats = self.arbiter.inbounds_outbound_check(outbound, self._args.license_expression)
                 compatible = (compats['compatibility'] == "Yes")
                 if compatible:
                     outbounds.append(outbound)
@@ -57,14 +54,13 @@ class FlictImpl:
                              f'Invalid license expression: {self._args.license_expression}')
 
     def list_licenses(self):
-        arbiter = self._get_arbiter()
-        licenses = list(arbiter.supported_licenses())
+        licenses = list(self.arbiter.supported_licenses())
         licenses.sort(key=str.lower)
         return self._formatter.format_support_licenses(licenses)
 
-    def _handle_lico_project(self, arbiter, reader, project_file, formatter):
+    def _handle_lico_project(self, reader, project_file, formatter):
         project = reader.read_project(project_file)
-        verification = arbiter.verify(project)
+        verification = self.arbiter.verify(project)
         verification_report = formatter.format_verification(verification)
         return verification_report
 
@@ -92,20 +88,19 @@ class FlictImpl:
         return arbiter
 
     def verify(self):
-        arbiter = self._get_arbiter()
         formatter = FormatterFactory.formatter(self._args.output_format)
 
         if self._args.out_license and self._args.in_license_expr:
-            compats = arbiter.inbounds_outbound_check(self._args.out_license, self._args.in_license_expr)
+            compats = self.arbiter.inbounds_outbound_check(self._args.out_license, self._args.in_license_expr)
             formatted = formatter.format_compatibilities(compats)
             return formatted
 
         elif self._args.verify_flict:
             #print("con flict: " + str(self._args.license_matrix_file))
             project_reader = ProjectReaderFactory.get_projectreader(self._args.verify_flict, None, "flict")
-            return self._handle_lico_project(arbiter, project_reader, self._args.verify_flict, formatter)
+            return self._handle_lico_project(project_reader, self._args.verify_flict, formatter)
 
         elif self._args.verify_sbom:
             project_reader = ProjectReaderFactory.get_projectreader(self._args.verify_sbom, self._args.sbom_dirs)
-            return self._handle_lico_project(arbiter, project_reader, self._args.verify_sbom, formatter)
+            return self._handle_lico_project(project_reader, self._args.verify_sbom, formatter)
 
