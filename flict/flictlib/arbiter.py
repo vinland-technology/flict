f17c3dd3b97af75a0511937b43cc3bacb8e77323
Sandklef Henrik <hesa@sandklef.com>
2022-10-10 20:43:29 +0200

    Arbiter: Add doc

--- flict/flictlib/arbiter.py
+++ flict/flictlib/arbiter.py
@@ -151,20 +151,24 @@ class Arbiter:
                 dep_infos.append(dep_info)
             package_info['dependencies'] = dep_infos
 
+            # Get a list of the outbound licenses for all packages
             outbound_licenses = self._top_package_license(package['license'], licenses, package_info, dep_infos)
             package_info['outbound_licenses'] = outbound_licenses
 
+            # Get the alias for all outbound licenses
             package_info['outbound_licenses_aliased'] = []
             for lic in outbound_licenses:
                 package_info['outbound_licenses_aliased'].append(self.license_compatibility.replace_aliases(lic))
 
-            # Chose outbound
-            chosen_alias = self.license_compatibility.chose_license(package_info['outbound_licenses_aliased'])
+            # Identify single outbound (chosen) license (from the aliased outbound licenses)
+            chosen_alias = self.license_compatibility.choose_license(package_info['outbound_licenses_aliased'])
             if chosen_alias is None:
                 chosen_license = None
                 package_info['outbound_license'] = None
                 package_info['outbound_license_aliased'] = None
             else:
+                # Find the index of the aliased license
+                # and use that to identify the original (not aliased) license
                 index = package_info['outbound_licenses_aliased'].index(chosen_alias)
                 chosen_license = outbound_licenses[index]
                 package_info['outbound_license'] = chosen_license
