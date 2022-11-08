4210e9f0ba67418833b64ced8a08f23c63557598
Sandklef Henrik <hesa@sandklef.com>
2022-11-08 22:52:24 +0100

    Test: inner packages in SPDX

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -71,6 +71,41 @@ class TestVerification(unittest.TestCase):
         self.assertEqual(len(compats), 4)
 
 
+    def test_zlib_no_proper_deps(self):
+        """This JSON file has one relationship, but not a valid one
+        """
+        reader = ProjectReaderFactory.get_projectreader(project_format="spdx", project_dirs=["example-data"])
+        zlib = reader.read_project("tests/zlib-no-deps-1.2.11.spdx.json")
+
+        # generic check
+        verification = self.arbiter.verify(zlib)
+        self._verify_verification(verification)
+
+        # both packages should contain 0 deps
+        # - zlib has a relationship that is not "valid"
+        # - zlib-doc has no relationships
+        for pkg in zlib['packages']:
+            self.assertEqual(len(pkg['dependencies']), 0)
+            
+    def test_zlib_with_deps(self):
+        """This JSON file has one valid relationship
+        """
+        reader = ProjectReaderFactory.get_projectreader(project_format="spdx", project_dirs=["example-data"])
+        zlib = reader.read_project("tests/zlib-with-deps-1.2.11.spdx.json")
+
+        verification = self.arbiter.verify(zlib)
+
+        # - zlib has relationships:
+        #      one "valid" and one invalid => 1 dep
+        # - zlib-libber has no relationships => 0 deps
+        for pkg in zlib['packages']:
+            if 'defined' in pkg and not pkg['defined']:
+                p = pkg['defined']
+            if pkg['name'] == 'SPDXRef-Package-zlib-libber':
+                self.assertEqual(len(pkg['dependencies']), 0)
+            elif pkg['name'] == 'SPDXRef-Package-zlib':
+                self.assertEqual(len(pkg['dependencies']), 1)
+
 if __name__ == '__main__':
     unittest.main()
     
