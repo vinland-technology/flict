9344a04ab20777f8deef717bd916db8d2886205f
Henrik Sandklef <hesa@sandklef.com>
2022-11-09 22:04:04 +0100

    Utils: add flict info to meta data

--- flict/flictlib/utils.py
+++ flict/flictlib/utils.py
@@ -5,12 +5,15 @@
 import datetime
 import os
 
+from flict.flictlib.flict_config import flict_version
+from flict.flictlib.flict_config import FLICT_HOMEPAGE
+from flict.flictlib.flict_config import FLICT_BUGS
 
 def timestamp():
     return str(datetime.datetime.now())
 
 
-def meta_information(start_time=""):
+def meta_information(start_time=''):
     uname = os.uname()
     return {
         'os': uname.sysname,
@@ -19,6 +22,15 @@ def meta_information(start_time=""):
         'machine': uname.machine,
         'host': uname.nodename,
         'user': os.environ.get('USER'),
-        "start_time": start_time,
+        'start_time': start_time,
         'stop_time': timestamp(),
+        'flict': flict_information()
+    }
+
+def flict_information():
+    return {
+        'tool': 'flict',
+        'version': flict_version,
+        'homepage': FLICT_HOMEPAGE,
+        'issues': FLICT_BUGS
     }
