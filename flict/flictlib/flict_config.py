36e3e38f4b5c519aa07ff6d4dc5331c989e7504e
Henrik Sandklef <hesa@sandklef.com>
2023-06-14 21:59:46 +0200

    default to JSON matrix
    Default format for extended matrix is JSON

--- flict/flictlib/flict_config.py
+++ flict/flictlib/flict_config.py
@@ -11,7 +11,7 @@
 import json
 import os
 
-from osadl_matrix import OSADL_MATRIX
+from osadl_matrix import OSADL_MATRIX_JSON
 
 
 def read_user_config():
@@ -38,5 +38,5 @@ VAR_DIR = os.path.join(SCRIPT_DIR, "var")
 BUILTIN_ALIAS_FILE = os.path.join(VAR_DIR, "alias.json")
 DEFAULT_FLICT_ALIAS_FILE = os.path.join(VAR_DIR, BUILTIN_ALIAS_FILE)
 
-DEFAULT_MATRIX_FILE = _userconfig.get('matrix-file', OSADL_MATRIX)
+DEFAULT_MATRIX_FILE = _userconfig.get('matrix-file', OSADL_MATRIX_JSON)
 DEFAULT_OUTPUT_FORMAT = _userconfig.get('output-format', "JSON")
