d5e9c9716b82e7f674f94fa0083e7a73ff919233
Henrik Sandklef <hesa@sandklef.com>
2024-01-10 00:32:59 +0100

    test: update tests for changes in flict

--- tests/test_simplified.py
+++ tests/test_simplified.py
@@ -17,10 +17,12 @@ def test_simplify():
     _test_expression(['MIT'], '{"original": "MIT", "simplified": "MIT"}')
     _test_expression(['MIT and MIT'], '{"original": "MIT and MIT", "simplified": "MIT"}')
     _test_expression(['MIT and MIT and BSD-3-Clause'], '{"original": "MIT and MIT and BSD-3-Clause", "simplified": "BSD-3-Clause AND MIT"}')
-    _test_expression(['BSD'], '{"original": "BSD", "simplified": "BSD-3-Clause"}')
-    _test_expression(['MIT and MIT and BSD'], '{"original": "MIT and MIT and BSD", "simplified": "BSD-3-Clause AND MIT"}')
-    _test_expression(['MIT and MIT or BSD'], '{"original": "MIT and MIT or BSD", "simplified": "BSD-3-Clause OR MIT"}')
+    _test_expression(['BSD3'], '{"original": "BSD3", "simplified": "BSD-3-Clause"}')
+    _test_expression(['MIT and MIT and BSD3'], '{"original": "MIT and MIT and BSD3", "simplified": "BSD-3-Clause AND MIT"}')
+    _test_expression(['MIT and MIT or BSD3'], '{"original": "MIT and MIT or BSD3", "simplified": "BSD-3-Clause OR MIT"}')
     _test_expression(['GPL-2.0-only'], '{"original": "GPL-2.0-only", "simplified": "GPL-2.0-only"}')
-    _test_expression(['GPL-2.0-or-later'], '{"original": "GPL-2.0-or-later", "simplified": "GPL-2.0-or-later"}')
-    _test_expression(['GPL-2.0-or-later and MIT'], '{"original": "GPL-2.0-or-later and MIT", "simplified": "GPL-2.0-or-later AND MIT"}')
+    _test_expression(['GPL-2.0-or-later'], '{"original": "GPL-2.0-or-later", "simplified": "GPL-2.0-only OR GPL-3.0-only"}')
+    _test_expression(['GPL-2.0-or-later and MIT'], '{"original": "GPL-2.0-or-later and MIT", "simplified": "MIT AND (GPL-2.0-only OR GPL-3.0-only)"}')
     _test_expression(['MIT and GPL-2.0-only WITH Classpath-exception-2.0 and MIT'], '{"original": "MIT and GPL-2.0-only WITH Classpath-exception-2.0 and MIT", "simplified": "GPL-2.0-only WITH Classpath-exception-2.0 AND MIT"}')
+
+
