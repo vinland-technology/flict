064697e87e9779d1d78550ed0b9b836682f49d83
Henrik Sandklef <hesa@sandklef.com>
2024-01-10 23:57:04 +0100

    format: remove helper function

--- flict/flictlib/format/common.py
+++ flict/flictlib/format/common.py
@@ -25,26 +25,3 @@ compat_interprets = {
         'question': {'markdown': 'Q---'},
     },
 }
-
-def extract_compatibilities(verification):
-    """
-    this method assumes 1 package with 1 dependency
-    """
-    compats = []
-    for package in verification['packages']:
-        dependency = package['dependencies'][0]
-        compats.append({
-            'name': package['name'],
-            'version': package['version'],
-            'original_outbound': package['original_license'],
-            'outbound': package['license'],
-            'inbound': dependency['license'],
-            'original_inbound': dependency['original_license'],
-            'result': {
-                'outbound_licenses': package['outbound_licenses'],
-                'allowed_outbound_licenses': package['allowed_outbound_licenses'],
-                'outbound_license': package['outbound_license'],
-                'problems': package['problems'],
-            },
-        })
-    return compats
