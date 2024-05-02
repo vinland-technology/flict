0ddb1a10e67bd8d943331bbc5a23de11d8c5a280
Konrad Weihmann <kweihmann@outlook.com>
2024-05-02 16:57:26 +0200

    factory: remove singleton pattern
    as this stopped to work on python 3.11 latest release
    in CI + it's wasn't very beneficial either, as the tool
    will have to create a new instance to the attribute less
    formatter class, hence it's save to remove it
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- flict/flictlib/format/factory.py
+++ flict/flictlib/format/factory.py
@@ -16,19 +16,12 @@ from flict.flictlib.format.markdown_format import PackageMarkdownFlictFormatter
 
 class FormatterFactory:
 
-    _instance = None
-
     @staticmethod
     def formatter(format_):
-        if FormatterFactory._instance is None:
-            if format_.lower() == "json":
-                FormatterFactory._instance = JsonFormatter()
-            elif format_.lower() == "text":
-                FormatterFactory._instance = TextFormatter()
-            elif format_.lower() == "dot":
-                FormatterFactory._instance = DotFormatter()
-            elif format_.lower() == "markdown":
-                FormatterFactory._instance = PackageMarkdownFlictFormatter()
-            # else ignored
-        # else ignored
-        return FormatterFactory._instance
+        if format_.lower() == "text":
+            return TextFormatter()
+        elif format_.lower() == "dot":
+            return DotFormatter()
+        elif format_.lower() == "markdown":
+            return PackageMarkdownFlictFormatter()
+        return JsonFormatter()
